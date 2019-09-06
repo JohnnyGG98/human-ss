8d0e4084282b6b44f0da30103b6e178914feddf3
paolaEs <46163863+paolaEs@users.noreply.github.com>
2019-09-05 20:24:09 -0500

    commit

--- src/main/java/com/shopshopista/humanss/model/Persona.java
+++ src/main/java/com/shopshopista/humanss/model/Persona.java
@@ -19,7 +19,6 @@ public class Persona implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "id_persona")
-<<<<<<< HEAD
 	private Long id_persona;	
 	
 	@Column(name = "per_identificacion", length = 15, nullable =false)	
@@ -45,41 +44,10 @@ public class Persona implements Serializable {
 	
 	@Column(name="per_fecha_registo", columnDefinition="timestamp DEFAULT now()")
 	private LocalDateTime per_fecha_registo;
-	
-	
-=======
-	private Long idPersona;
-
-	@Column(name = "per_identificacion", length = 15, nullable = false)
-	private String identificacionPersona;
-
-	@Column(name = "per_primer_nombre", columnDefinition = "character varying(75) DEFAULT ''")
-	private String primerNombrePersona;
-
-	@Column(name = "per_segundo_nombre", length = 75, nullable = false)
-	private String segundoNombrePersona;
-
-	@Column(name = "per_primer_apellido", length = 75, nullable = false)
-	private String primerApellidoPersona;
-
-	@Column(name = "per_segundo_apellido", length = 75)
-	private String segundoApellidoPersona;
-
-	@Column(name = "per_correo", length = 50, nullable = false)
-	private String correoElectronicoPersona;
-
-	@Column(name = "per_sexo", length = 50, nullable = false)
-	private String sexoPersona;
-
-	@Column(name = "per_fecha_registo", columnDefinition = "timestamp DEFAULT now()")
-	private LocalDate fechaRegistroPersona;
->>>>>>> f2e6069f05522f8ed301e07718609b264051c9bd
 
 	public Persona() {
 
 	}
-
-<<<<<<< HEAD
 
 	public Persona(Long id_persona, String per_identificacion, String per_primer_nombre, String per_segundo_nombre,
 			String per_primer_apellido, String per_segundo_apellido, String per_correo, String per_sexo,
@@ -201,104 +169,9 @@ public class Persona implements Serializable {
 
 	public void setPer_fecha_registo(LocalDateTime per_fecha_registo) {
 		this.per_fecha_registo = per_fecha_registo;
-=======
-	public Persona(Long idPersona, String identificacionPersona, String primerNombrePersona,
-			String segundoNombrePersona, String primerApellidoPersona, String segundoApellidoPersona,
-			String correoElectronicoPersona, String sexoPersona, LocalDate fechaRegistroPersona) {
-
-		this.idPersona = idPersona;
-		this.identificacionPersona = identificacionPersona;
-		this.primerNombrePersona = primerNombrePersona;
-		this.segundoNombrePersona = segundoNombrePersona;
-		this.primerApellidoPersona = primerApellidoPersona;
-		this.segundoApellidoPersona = segundoApellidoPersona;
-		this.correoElectronicoPersona = correoElectronicoPersona;
-		this.sexoPersona = sexoPersona;
-		this.fechaRegistroPersona = fechaRegistroPersona;
 	}
 
-	public Long getIdPersona() {
-		return idPersona;
-	}
 
-	public void setIdPersona(Long idPersona) {
-		this.idPersona = idPersona;
-	}
-
-	public String getIdentificacionPersona() {
-		return identificacionPersona;
-	}
-
-	public void setIdentificacionPersona(String identificacionPersona) {
-		this.identificacionPersona = identificacionPersona;
-	}
-	
-	public String getPrimerNombrePersona() {
-		return primerNombrePersona;
-	}
-
-	public void setPrimerNombrePersona(String primerNombrePersona) {
-		this.primerNombrePersona = primerNombrePersona;
-	}
-
-	public String getSegundoNombrePersona() {
-		return segundoNombrePersona;
-	}
-
-	public void setSegundoNombrePersona(String segundoNombrePersona) {
-		this.segundoNombrePersona = segundoNombrePersona;
-	}
-
-	public String getPrimerApellidoPersona() {
-		return primerApellidoPersona;
-	}
-
-	public void setPrimerApellidoPersona(String primerApellidoPersona) {
-		this.primerApellidoPersona = primerApellidoPersona;
-	}
-
-	public String getSegundoApellidoPersona() {
-		return segundoApellidoPersona;
-	}
-
-	public void setSegundoApellidoPersona(String segundoApellidoPersona) {
-		this.segundoApellidoPersona = segundoApellidoPersona;
-	}
-
-	public String getCorreoElectronicoPersona() {
-		return correoElectronicoPersona;
-	}
-
-	public void setCorreoElectronicoPersona(String correoElectronicoPersona) {
-		this.correoElectronicoPersona = correoElectronicoPersona;
-	}
-
-	public String getSexoPersona() {
-		return sexoPersona;
-	}
-
-	public void setSexoPersona(String sexoPersona) {
-		this.sexoPersona = sexoPersona;
-	}
-
-	public LocalDate getFechaRegistroPersona() {
-		return fechaRegistroPersona;
-	}
-
-	public void setFechaRegistroPersona(LocalDate fechaRegistroPersona) {
-		this.fechaRegistroPersona = fechaRegistroPersona;
->>>>>>> f2e6069f05522f8ed301e07718609b264051c9bd
-	}
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
-
-<<<<<<< HEAD
-
-
-=======
->>>>>>> f2e6069f05522f8ed301e07718609b264051c9bd
 	@Override
 	public String toString() {
 		return "Persona [id_persona=" + id_persona + ", per_identificacion=" + per_identificacion
@@ -308,11 +181,5 @@ public class Persona implements Serializable {
 				+ "]";
 	}
 
-<<<<<<< HEAD
-
 	
-
-		
-=======
->>>>>>> f2e6069f05522f8ed301e07718609b264051c9bd
 }
