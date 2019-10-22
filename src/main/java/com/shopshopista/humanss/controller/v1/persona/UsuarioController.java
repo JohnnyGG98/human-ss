8c9d459d0288634152cde8d75b07a352b81afd4b
AndresUV7 <andres.ullauri18@gmail.com>
2019-10-22 00:24:03 -0500

    Agregando metodos al controlador de Usuario

--- src/main/java/com/shopshopista/humanss/controller/v1/persona/UsuarioController.java
+++ src/main/java/com/shopshopista/humanss/controller/v1/persona/UsuarioController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shopshopista.humanss.model.cliente.Cliente;
+import com.shopshopista.humanss.model.cliente.Preferencias;
 import com.shopshopista.humanss.model.persona.Usuario;
 import com.shopshopista.humanss.repository.persona.IUsuarioRepository;
 import com.shopshopista.humanss.services.persona.PersonaService;
@@ -19,6 +20,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -46,6 +48,16 @@ public class UsuarioController {
     @PostMapping("/editar")
     public Usuario editar(@RequestBody @Valid Usuario u) {
         return PS.guardarUsuario(u);
+    }
+    
+    @PutMapping(path = {"/actualizar/{id}"})
+    public Usuario actualizar(@RequestBody Usuario u, @PathVariable("id") Long id) {
+        return PS.guardarUsuario(u);
+    }
+    
+    @GetMapping(path = {"/{id}"})
+    public Usuario seleccionarId(@PathVariable("id") Long id) {
+        return this.usuarioRepositorio.getOne(id);
     }
     
     @GetMapping("/buscar/{nick}")
